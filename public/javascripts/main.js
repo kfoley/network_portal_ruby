63fbbff51c3e09dbcb38bdcf2452818d0f744914
kfoley <kfoley73@gmail.com>
2011-09-08 10:44:41 -0700

    Have two networks working each with their own set of filters and in their own tab.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -63,6 +63,8 @@ $(document).ready(function() {
         $(".toggle_container").slideToggle("slow");
     });
 
+
+    // VIS-1 STUFF
 	$("#slider-residual").slider({
 	    value: 0.4,
 	    min: 0.2,
@@ -138,6 +140,85 @@ $(document).ready(function() {
 
 	$("input:button, a, button", "#filter-section").button();
 	$("#buttonpanel").buttonset();
+
+
+	// VIS_2 stuff
+	$("#slider-residual2").slider({
+	    value: 0.4,
+	    min: 0.2,
+	    max: 0.6,
+	    step: 0.05,
+	    change: function(event, ui) {
+	            $("#residual2").val(ui.value);
+	    }
+	}).bind({
+	    slidechange: function(event, ui) {
+	                  vis_2.filter("nodes", function(node) { return node.data.resid <= ui.value; }, false );
+			  //vis_2.layout('Preset');
+			  //vis_2.visualStyle();
+	    }
+	});
+
+	$("#residual2").val($("#slider-residual2").slider("value"));
+
+	$("#slider-weight2").slider({
+	    value: 0,
+	    min: -0.3,
+	    max: 0.3,
+	    step: 0.05,
+	    change: function(event, ui) {
+	            $("#weight2").val(ui.value);
+	    }
+	}).bind({
+	    slidechange : function(event, ui) {
+	                  vis_2.filter("edges", function(edge) { return edge.data.weight <= ui.value; }, false );             
+	                  //vis.filter("edges", function(edge) { return edge.data.weight <= $("#weight").val(); }, false );             
+			  //vis.layout('Preset');
+			  //vis.visualStyle();
+	    }
+	});
+	$("#weight2").val($("#slider-weight2").slider("value"));
+
+	$("#reset_residual2").click(function() {
+	    $("#slider-residual2").slider( { value: 0.4 } );
+	    vis_2.filter("nodes", function(node) { return lookup2[node.data.id] == "true"; } );
+	});
+
+	$("#reset_weight2").click(function() {
+	    $("#slider-weight2").slider( { value: 0 } );
+	    vis_2.removeFilter("edges", false);
+	});
+
+	$("#hide_bicluster2").click(function() {
+	    vis_2.filter("nodes", function(node) { return node.data.type != "bicluster"; }, false );
+	    vis_2.layout('Preset');
+	    vis_2.visualStyle();
+	});
+
+	$("#hide_andgate2").click(function() {
+	    vis_2.filter("nodes", function(node) { return node.data.type != "AND_Gate"; }, false );
+	    vis_2.layout('Preset');
+	    vis_2.visualStyle();
+	});
+
+	$("#hide_regulator2").click(function() {
+	    vis_2.filter("nodes", function(node) { return node.data.type != "regulator"; }, false );
+	    vis_2.layout('Preset');
+	    vis_2.visualStyle();
+	});
+
+	$("#hide_genes2").click(function() {
+	    vis_2.filter("nodes", function(node) { return node.data.type != "gene"; }, false );
+	    vis_2.layout('Preset');
+	    vis_2.visualStyle();
+	});
+
+	$("#reset_all2").click(function() {
+	    vis_2.filter("nodes", function(node) { return lookup2[node.data.id] == "true"; } );
+	});
+
+	$("input:button, a, button", "#filter-section2").button();
+	$("#buttonpanel2").buttonset();
 
 	
 });
